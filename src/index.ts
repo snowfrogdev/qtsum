f18147eeea2972d729b55f7f3172b33fda8f41fc
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-04-28 15:45:16 -0400

    Make a header and image box

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 // Import stylesheets
 import './style.css';
 
+/*
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = '<h1>TypeScript Starter - qtsum</h1>';
+const appDiv: HTMLElement = document.getElementById('app') as HTMLElement;
+appDiv.innerHTML = '<h1>TypeScript Starter - qtsum.</h1>';
+*/
